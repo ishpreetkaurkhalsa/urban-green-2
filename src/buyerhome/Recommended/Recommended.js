aad756aa5a69a481b9fff8287ba16357ec30b72e
Ishpreet Kaur Khalsa <113664759+ishpreetkaurkhalsa@users.noreply.github.com>
2024-03-31 06:59:53 +0530

    Update

--- src/buyerhome/Recommended/Recommended.js
+++ src/buyerhome/Recommended/Recommended.js
@@ -1,18 +1,42 @@
-import Button from "../components/Button";
-import "./Recommended.css";
+// import Button from "../components/Button";
+// import "./Recommended.css";
+
+// const Recommended = ({ handleClick }) => {
+//   return (
+//     <div className="outer" >
+//       <div style={{width:'100vw',backgroundColor:'white',paddingBottom:'15px',position:'fixed',marginTop:'10px'}}>
+//         <h2 className="recommended-title">Recommended</h2>
+//         <div className="recommended-flex">
+//           <Button onClickHandler={handleClick} value="" title="All Products" />
+//           <Button onClickHandler={handleClick} value="Terrace" title="Terrace" />
+//           <Button onClickHandler={handleClick} value="Open Land" title="Open Land" />
+//           <Button onClickHandler={handleClick} value="Balcony" title="Balcony" />
+//           <Button onClickHandler={handleClick} value="Veranda" title="Veranda" />
+//           <Button onClickHandler={handleClick} value="Apartment" title="Apartment" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Recommended;
+import React, { useState, useEffect } from 'react';
+import Button from '../components/Button';
+import './Recommended.css';
 
 const Recommended = ({ handleClick }) => {
+  
   return (
-    <div className="outer" >
-      <div style={{width:'100vw',backgroundColor:'white',paddingBottom:'15px',position:'fixed'}}>
+    <div className="outer">
+      <div style={{ width: '100vw', backgroundColor: 'white', paddingBottom: '15px', position: 'fixed', marginTop: '10px' }}>
         <h2 className="recommended-title">Recommended</h2>
-        <div className="recommended-flex">
-          <Button onClickHandler={handleClick} value="" title="All Products" />
-          <Button onClickHandler={handleClick} value="Terrace" title="Terrace" />
-          <Button onClickHandler={handleClick} value="Open Land" title="Open Land" />
-          <Button onClickHandler={handleClick} value="Balcony" title="Balcony" />
-          <Button onClickHandler={handleClick} value="Veranda" title="Veranda" />
-          <Button onClickHandler={handleClick} value="Apartment" title="Apartment" />
+        <div className="recommended-flex" data-aos="zoom-in"> 
+          <Button onClickHandler={handleClick} value="" title="All Products" className={`button`} />
+          <Button onClickHandler={handleClick} value="Terrace" title="Rice" className={`button `} />
+          <Button onClickHandler={handleClick} value="Open Land" title="Wheat" className={`button `} />
+          <Button onClickHandler={handleClick} value="Balcony" title="Maize" className={`button `} />
+          <Button onClickHandler={handleClick} value="Veranda" title="Cotton" className={`button `} />
+          <Button onClickHandler={handleClick} value="Apartment" title="Soybean" className={`button `} />
         </div>
       </div>
     </div>
